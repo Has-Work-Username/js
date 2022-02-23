7be42405b0becf2fcd25fcd1160354507cf74a8d
Said-akbar-7 <nabixojaevsaidakbar@gmail.com>
2022-02-23 21:26:03 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+ let num = prompt("Sonni Kiriting")
+
+    if (num % 5 === 0 && num % 3 === 0) {
+       console.log("Fizz Buzz");
+    }   
+    else if (num % 3 === 0) {
+       console.log("Buzz");
+    }   
+    else if (num % 5 === 0) {
+       console.log("Fizz");
+    }   
+    else {
+       console.log(num);
+    }    
+
+
+
+   
